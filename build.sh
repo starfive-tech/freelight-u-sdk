643391c8bc079078a45f71cc785ce1508f7b0abe
Stephen L Arnold <nerdboy@gentoo.org>
2021-05-06 15:08:41 -0700

    make jenkins build happy at least once...
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- build.sh
+++ build.sh
@@ -2,9 +2,13 @@
 
 CORES=$(getconf _NPROCESSORS_ONLN)
 
+if [ -d ./build-sdk ] ; then
+    rm -rf ./build-sdk
+fi
+
 mkdir ./build-sdk && cd build-sdk
 
-repo init -u https://github.com/starfive-tech/freelight-u-sdk.git -b starfive -m tools/manifests/riscv-sdk.xml --no-clone-bundle --depth=1
+repo init -u https://github.com/sarnold/freelight-u-sdk.git -b manifest-pr -m tools/manifests/riscv-sdk.xml --no-clone-bundle --depth=1
 repo sync --no-tags --no-clone-bundle --current-branch
 
 cd freelight-u-sdk/
