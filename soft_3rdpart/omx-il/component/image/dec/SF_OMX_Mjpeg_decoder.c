476867c2c820d6199d86a7c92076e69d570393a5
sw.multimedia <sw.multimedia@starfivetech.com>
2022-04-19 17:22:56 +0800

    sf-omx-il: Add OMX_COLOR_FormatYVU420PackedPlanar for NV21 Signed-off-by: sw.multimedia <sw.multimedia@starfivetech.com>

--- soft_3rdpart/omx-il/component/image/dec/SF_OMX_Mjpeg_decoder.c
+++ soft_3rdpart/omx-il/component/image/dec/SF_OMX_Mjpeg_decoder.c
@@ -317,7 +317,7 @@ static OMX_ERRORTYPE SF_OMX_GetParameter(
             portFormat->eColorFormat = OMX_COLOR_FormatYUV420SemiPlanar; //NV12
             break;
         case 4:
-            portFormat->eColorFormat = OMX_COLOR_FormatYVU420PackedSemiPlanar; //NV21
+            portFormat->eColorFormat = OMX_COLOR_FormatYVU420PackedPlanar; //NV21
             break;
         default:
             if (index > 0)
@@ -443,6 +443,7 @@ static OMX_ERRORTYPE SF_OMX_SetParameter(
                         pOutputPort->nBufferSize = (width * height * 3) / 2;
                         break;
                     case OMX_COLOR_FormatYVU420PackedSemiPlanar: //NV21
+                    case OMX_COLOR_FormatYVU420PackedPlanar:
                         decConfig->cbcrInterleave = CRCB_INTERLEAVE;
                         decConfig->packedFormat = PACKED_FORMAT_NONE;
                         pOutputPort->nBufferSize = (width * height * 3) / 2;
@@ -488,6 +489,7 @@ static OMX_ERRORTYPE SF_OMX_SetParameter(
             pPort->format.video.eColorFormat = portFormat->eColorFormat;
             break;
         case OMX_COLOR_FormatYVU420PackedSemiPlanar: //NV21
+        case OMX_COLOR_FormatYVU420PackedPlanar:
             decConfig->cbcrInterleave = CRCB_INTERLEAVE;
             decConfig->packedFormat = PACKED_FORMAT_NONE;
             pSfMjpegImplement->frameFormat = FORMAT_420;
